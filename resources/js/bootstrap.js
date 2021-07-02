7cf2ce46e1a7b2fe9d98f5cd36a92724e5041dd0
Andrey <dev@sothis.com.br>
2021-07-02 11:39:55 -0300

    minorfix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -22,7 +22,7 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+window.axios.defaults.baseURL = 'http://transicacao.sothistelecom.com/';
 
 
 /**
